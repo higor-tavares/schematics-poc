40dc6e3b835210a07b3c42d727884b4552846aa6
paulo.tavares <paulo.tavares@olxbr.com>
2024-03-29 13:49:00 -0300

    remove unecessary imports

--- src/person.py
+++ src/person.py
@@ -1,7 +1,5 @@
 from schematics.models import Model
 from schematics.types import StringType, URLType
-from schematics.exceptions import DataError
-import json
 
 class Person(Model):
     name = StringType(required=True)
